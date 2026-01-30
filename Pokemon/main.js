baec95442783af04559274b3854326ddc9704c06
arkanandi08 <nandiarka2008@gmail.com>
2026-01-30 15:31:43 +0530

    update

--- Pokemon/main.js
+++ Pokemon/main.js
@@ -1,1 +1,31 @@
+function fullscreen() {
+  const elem = document.documentElement;
 
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { // Safari
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE11
+    elem.msRequestFullscreen();
+  }
+  document.getElementById("full").style.display="none";
+  document.getElementById("exfull").style.display="flex";
+}
+function exfull() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+  document.getElementById("exfull").style.display="none";
+
+  document.getElementById("full").style.display="flex";}
+
+  function aclick(){
+    a=document.getElementById("ifrm").innerText;
+    if(a=="<iframe src='start.html' id='frame'></iframe>"){
+      document.getElementById("ifrm").innerText="<iframe src='intro.html' id='frame'></iframe>"
+    }
+  }
