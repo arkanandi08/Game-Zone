958f5c51feecb56c51f860e2074a3cba6b9150b4
arkanandi08 <nandiarka2008@gmail.com>
2026-01-31 21:15:33 +0530

    final

--- Pokemon/main.js
+++ Pokemon/main.js
@@ -1,6 +1,13 @@
+let iframe;
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("frame")) {
+    iframe = document.getElementById("frame").contentWindow;
+  }
+});
+
 function fullscreen() {
   const elem = document.documentElement;
-
+  
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
   } else if (elem.webkitRequestFullscreen) { // Safari
@@ -8,9 +15,10 @@ function fullscreen() {
   } else if (elem.msRequestFullscreen) { // IE11
     elem.msRequestFullscreen();
   }
-  document.getElementById("full").style.display="none";
-  document.getElementById("exfull").style.display="flex";
+  document.getElementById("full").style.display = "none";
+  document.getElementById("exfull").style.display = "flex";
 }
+
 function exfull() {
   if (document.exitFullscreen) {
     document.exitFullscreen();
@@ -19,13 +27,37 @@ function exfull() {
   } else if (document.msExitFullscreen) {
     document.msExitFullscreen();
   }
-  document.getElementById("exfull").style.display="none";
+  document.getElementById("exfull").style.display = "none";
+  
+  document.getElementById("full").style.display = "flex";
+}
 
-  document.getElementById("full").style.display="flex";}
+function stclk() {
+  const a = document.getElementById("frame");
+  const src = a.getAttribute("src");
+  if (src == "start.html") {
+    a.setAttribute("src", "intro.html");
+  }
+}
 
-  function aclick(){
-    a=document.getElementById("ifrm").innerText;
-    if(a=="<iframe src='start.html' id='frame'></iframe>"){
-      document.getElementById("ifrm").innerText="<iframe src='intro.html' id='frame'></iframe>"
+function aclk() {
+  const a = document.getElementById("frame");
+  const src = a.getAttribute("src");
+  if (src == "intro.html") {
+    let divs = iframe.document.querySelectorAll(".step");
+    for (let i = 0; i < divs.length; i++) {
+      if (divs[i].classList.contains("active")) {
+        var c = i;
+        break;
+      }
+      else {
+        var c = divs.length-1;
+      }
+    }
+    divs[c].classList.remove("active");
+    c++;
+    if (c < divs.length) {
+      divs[c].classList.add("active");
     }
   }
+}
